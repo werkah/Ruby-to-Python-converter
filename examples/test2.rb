64c3ae461fb0805165956bfd68e3e16bfc9082d6
kSuroweczka <92479853+kSuroweczka@users.noreply.github.com>
2023-05-08 20:30:39 +0200

    tests fixed

--- examples/test2.rb
+++ examples/test2.rb
@@ -1,12 +1,7 @@
-$i = 0
-$num = 5
+i = 0
+num = 5
 
-while $i < $num  do
+while i < num  do
    puts("while - ok")
-   $i +=1
+   i +=1
 end
-
-begin
-   puts("do while - ok")
-   $i +=1
-end while $i < $num
--- examples/test3.rb
+++ examples/test3.rb
@@ -1,12 +1,7 @@
-$i = 0
-$num = 5
+i = 0
+num = 5
 
-until $i > $num  do
+until i > num  do
    puts("until - ok")
-   $i +=1;
+   i +=1;
 end
-
-begin
-   puts("do until - ok")
-   $i +=1;
-end until $i > $num
